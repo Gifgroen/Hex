a86be184060e0fd5b0ba8638d6d4b7fd47173297
Karsten Westra <karstenwestra@gmail.com>
2020-09-17 16:51:10 +0200

    Aded blank Unity v2019.4.10f1 Project with basic structure.

---
+++ HexUnity/Assets/Gifgroen/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Gifgroen
+{
+    public class GameController : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Start()");
+        }
+
+        private void Update()
+        {
+            Debug.Log("Update()");
+        }
+    }
+}
